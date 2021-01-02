a6635f6b21615bba41350f1c18cd7f741e7536e8
Rob V <rvans003@fiu.edu>
2021-01-02 18:14:18 -0500

    added code for when special characters user input is true

--- Develop/script.js
+++ Develop/script.js
@@ -135,6 +135,40 @@ function generatePassword(){
       return pwd;
       }
 
+    //special character true choice variations 
+
+    //special character only true condition
+
+    if(lowerCase == false && (upperCase == false) && (numericValue == false) && (specialCharacter == true)){
+      charset = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
+
+    //special character, lower case, and upper case true, numeric value false condition
+    if(lowerCase == true && (upperCase == true) && (numericValue == false) && (specialCharacter == true)){
+      charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
+
+
+    //special character and lower case true, upper case and numeric value false condition
+    if(lowerCase == true && (upperCase == false) && (numericValue == false) && (specialCharacter == true)){
+      charset = "abcdefghijklmnopqrstuvwxyz !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
+
 
   }else(alert("please enter a valid numeric value between 8 and 128"))
 
