67d3b0155e755bdd78dd6860b3b6bd0fc0dd1160
Rob V <rvans003@fiu.edu>
2021-01-02 17:25:55 -0500

    added code to lowercase conditions , tested in default browser created the for loop that would return a random variable from a character set that is defined based on conditional statement (user input) assigned value to pwd variable and returned it when loop finished

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,57 @@ function generatePassword(){
   //declare length variable that will accecpt user input
   var length = prompt("Please enter a number between 8 and 128")
 
-  //set condition for function to run within certain numbers
+  //set condition for function to run within certain numbers (8 and 128)
   if(length >=8 && length <= 128){
 
-    //TODO: code password generation code
+    //prompt user for what they want in their password
+    //lower case, upper case, numbers, or special characters
+    var lowerCase = confirm("Do you want lowercase letters?")
+    var upperCase = confirm("Do you want uppercase letters?")
+    var numericValue = confirm("Do you want numbers in your password?")
+    var specialCharacter = confirm("Do you want special characters in your password?")
+
+  //lower case choice variations 
+
+    //only lower case characters true condition
+    if(lowerCase == true && (upperCase == false) && (numericValue==false) && (specialCharacter==false)){
+      charset = "abcdefghijklmnopqrstuvwxyz",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
+
+    //lower case and upper case true condition, numeric and special false condition
+    if(lowerCase == true && (upperCase == true) && (numericValue == false) && (specialCharacter == false)){
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
+
+    //lower case, upper case, and numeric true ; special false condition
+    if(lowerCase == true && (upperCase == true) && (numericValue == true) && (specialCharacter == false)){
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
+
+    //lower case, special, and numeric true ; upper case false condition
+    if(lowerCase == true && (upperCase == false) && (numericValue == true) && (specialCharacter == true)){
+      charset = "abcdefghijklmnopqrstuvwxyz0123456789 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+      pwd = "";
+      for (var i = 0, n = charset.length; i < length; i++) {
+          pwd += charset.charAt(Math.floor(Math.random() * n));
+        }
+      return pwd;
+      }
 
   }else(alert("please enter a valid numeric value between 8 and 128"))
 
