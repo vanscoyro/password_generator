d53787f8715be77c08fb356732c0c4c8fe5da0b2
Rob V <rvans003@fiu.edu>
2021-01-02 17:06:42 -0500

    added generatePassword function and a condition to validate user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//generate password function that takes user input and creates a password
+function generatePassword(){
+
+  //declare length variable that will accecpt user input
+  var length = prompt("Please enter a number between 8 and 128")
+
+  //set condition for function to run within certain numbers
+  if(length >=8 && length <= 128){
+
+    //TODO: code password generation code
+
+  }else(alert("please enter a valid numeric value between 8 and 128"))
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
